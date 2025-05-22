84961893c5a1aadf7e81366ed0da24a8457bed0c
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-05-22 09:12:13 -0300

    setup: cria a modelagem das entidades

---
+++ src/interface/characters.interface.ts
@@ -0,0 +1,10 @@
+import Planets from "./planets.interface";
+
+interface Characters {
+  name: String;
+  specie: String;
+  afiliation: String;
+  homeworld: Planets;
+}
+
+export default Characters;
---
+++ src/interface/starSystems.interface.ts
@@ -0,0 +1,9 @@
+import Planets from "./planets.interface";
+
+interface StarSystems {
+  name: String;
+  description: String;
+  planetList: Planets;
+}
+
+export default StarSystems;
